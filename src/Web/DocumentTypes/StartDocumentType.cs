48bd9041fc9ba57e44dd83ffc2c50b23d5b6bd23
vikekh <621212+vikekh@users.noreply.github.com>
2018-04-18 00:48:46 +0200

    StartDocumentType

---
+++ src/Web/DocumentTypes/StartDocumentType.cs
@@ -0,0 +1,12 @@
+﻿using Vega.USiteBuilder;
+
+namespace Vikekh.Www.Web.DocumentTypes
+{
+    [DocumentType(
+        Name = "Start Document Type",
+        AllowAtRoot = true
+    )]
+    public class StartDocumentType : DocumentTypeBase
+    {
+    }
+}
