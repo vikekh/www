5473c16d38b4376a31d10e457b8deb607ef7b459
vikekh <621212+vikekh@users.noreply.github.com>
2018-05-10 13:50:31 +0200

    Generic repository interface

---
+++ src/Vikekh.Www.Core/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Vikekh.Www.Core.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> List();
+        bool Create(T item);
+        bool Delete(int id);
+        T Get(int id);
+        bool SaveChanges();
+    }
+}
